c6a2411074ad094534d34d12e4b62cb3113f0cc5
Juwan <sirius_75@naver.com>
2023-02-11 10:57:03 +0900

    7.state

--- src/App 6_event.js
+++ src/App 6_event.js
@@ -1,6 +1,5 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
 
 function Header(props) {
   return <header>
@@ -18,7 +17,7 @@ function Nav(props) {
     lis.push(<li key={t.id}>
       <a id={t.id} href={'/read'+t.id} onClick={event=>{
       event.preventDefault();
-      props.onChangeMode(Number(event.target.id));
+      props.onChangeMode(event.target.id);
     }}>{t.title}</a>
     </li>)
   }
@@ -37,44 +36,21 @@ function Article(props) {
 }
 
 function App() {
-  //const _mode = useState('WELCOME');  // 'WELCOME'은 state의 초기값
-  //const mode =_mode[0];               // state의 값은 0번째 index로 읽는다.
-  //const setMode = _mode[1];           // state를 바꿀때는 1번째 index의 함수를 사용한다.
-
-  const [mode, setMode] = useState('WELCOME');  // 위 함수의 축약형
-  const [id, setId] = useState(null);
-
   const topics = [
     {id:1, title:'html', body:'html is...'},
     {id:2, title:'css', body:'css is...'},
     {id:3, title:'javascriptss', body:'javascript is...'}
   ]
 
-  let content = null;
-  if(mode==="WELCOME"){
-    content = <Article title="Welcome" body="Hello, web"></Article>
-  }
-  else if (mode === 'READ') {
-    let title, body = null;
-    for (let i = 0; i < topics.length; i++) {
-      if (topics[i].id === id) {
-        title = topics[i].title;
-        body = topics[i].body;
-      }
-    }
-    content = <Article title={title} body={body}></Article>
-  }
-
   return (
     <div>
       <Header title="WEB" onChangeMode={()=>{
-        setMode('WELCOME');
+        alert('Header');
       }}></Header>
       <Nav topics={topics} onChangeMode={(_id)=>{
-        setMode('READ');
-        setId(_id);
+        alert(_id);
       }}></Nav>
-      {content}
+      <Article title="Welcome" body="hello, web"></Article>
     </div>
   );
 }
